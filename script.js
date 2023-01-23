038464a57285240a3954f7b9d1132eed6f5d14da
Ma-Add Al-Jabouri <meljaboury@outlook.com>
2023-01-23 03:38:45 +0000

    MVP mostly complete just finishing up final few touches, draw functionality added, mostly DOM and CSS stuff left to do

--- script.js
+++ script.js
@@ -23,21 +23,28 @@ boardSquares.forEach(boardSquare => {
     boardSquare.addEventListener('click', onClick, { once: true })
 })
 
+
+
 function onClick(e) {
     const square = e.target
     const currentTurn = circlesTurn ? classCircle : classX
     square.innerHTML = currentTurn
-    swapTurn()
     if (checkForWinner (currentTurn)) {
-        console.log('winner')
+        document.getElementById("game-state").innerHTML = currentTurn + ' Wins!';
+    } else if (checkForDraw("board-square")) {
+        console.log('draw')
+    } else {
+        swapTurn()
     } 
-    // takeTurn (boardSquare)
+    
 }
 
 swapTurn = () => {
     circlesTurn = !circlesTurn
 }
 
+
+// Logic to check for a winner, by comparing entries in array to preset combination of wins.
 checkForWinner = (currentTurn) => {
     return winningCombos.some(combination => {
         return combination.every(index => {
@@ -45,3 +52,14 @@ checkForWinner = (currentTurn) => {
         })
     })
 }
+
+// Logic to check for a draw, by checking that every div element has been filled without a winner.
+function checkForDraw() {
+    var divs = document.getElementsByClassName("board-square");
+    for (var i = 0; i < divs.length; i++) {
+      if (divs[i].innerHTML.trim() === "") {
+        return false;
+      }
+    }
+    return true;
+  }
